27cd863e4c3a36b7e4d2150c79b7b1ec5afa1247
Mayank Ary <mayank.ary@finoit.co.in>
2024-02-15 14:53:58 +0530

    new filter added

--- doctorappointmentsystem/appointment/filters.py
+++ doctorappointmentsystem/appointment/filters.py
@@ -1,7 +1,12 @@
 import django_filters
-from .models import Doctor
+from .models import Doctor, Appointment
 
 class DoctorFilter(django_filters.FilterSet):
     class Meta:
         model = Doctor
         fields = ['first_name', 'last_name', 'specialization', 'location']
+        
+class AppointmentFilter(django_filters.FilterSet):
+    class Meta:
+        model = Appointment
+        fields = ['start_time', 'end_time', 'date', 'is_cancelled']
