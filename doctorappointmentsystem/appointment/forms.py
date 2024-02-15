c17e62a2bb883e7df326bab85612ba83187289b8
Mayank Ary <mayank.ary@finoit.co.in>
2024-02-15 14:54:21 +0530

    new feature added in form

--- doctorappointmentsystem/appointment/forms.py
+++ doctorappointmentsystem/appointment/forms.py
@@ -6,3 +6,8 @@ class DoctorReviewForm(forms.ModelForm):
     class Meta:
         model = DoctorReview
         fields = ['review']
+
+class AppointmentCreateFormDoctor(forms.ModelForm):
+    class Meta:
+        model = Appointment
+        fields = ['start_time', 'end_time', 'date']
