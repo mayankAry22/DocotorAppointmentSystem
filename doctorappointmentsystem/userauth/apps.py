575a94f6b6f5ef4f5a1e29e1992afb7f37dc9603
Mayank Ary <mayank.ary@finoit.co.in>
2024-02-16 15:33:15 +0530

    signal added to send message

--- doctorappointmentsystem/userauth/apps.py
+++ doctorappointmentsystem/userauth/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
 
-
 class UserauthConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'userauth'
+    
+    def ready(self):
+        import userauth.signals  # Add this line to import the signals.py
+
+    
